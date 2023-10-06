ea60ee6d885b09ad25ee8edcea6111010600c803
Sahid Castillo <almostsahid@protonmail.com>
2023-10-06 17:33:52 -0500

    Enhance and comment PipeWire script

--- pipewire.fish
+++ pipewire.fish
@@ -1,9 +1,11 @@
-if pidof pipewire 
-    if zenity --question --text="PipeWire is already running.\nDo you want to restart it?"
-        killall pipewire &
-        sleep 1
+#!/usr/bin/fish
+# Script to restart PipeWire. I don't use this anymore
+# Requires Zenity installed
+
+if pidof -q pipewire 
+    and zenity --question --text="PipeWire is already running.\nDo you want to restart it?" # Spawns a GTK dialog box
+        killall pipewire 
         pipewire &
-    end
 else
-	pipewire &
+    echo "Run Pipewire &"
 end
