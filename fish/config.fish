922508cccb8c854dfee63a2733c80db8f4304ceb
Sahid Castillo <almostsahid@protonmail.com>
2023-10-13 11:18:10 -0500

    Comment out environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
-# This disables the Fish welcoming message
+# This disables the Fish welcome message
 
 set fish_greeting ""
 
-# This will only run when Fish starts interactively. 
+# This will only run when Fish starts interactively
 # This sets a few aliases, you can change accordingly to your package manager
 
 if status is-interactive
@@ -16,45 +16,44 @@ if status is-interactive
     alias neo=neofetch
     alias :q=exit
     alias gs="git status"
+    alias off="doas poweroff"
+    alias rest="doas reboot"
 end
 
-# Here is a little script to create a RUNTIME DIR. If you are using an distro with Systemd, you probably won't need this
+# Here is a little script to create a RUNTIME DIR. If you are using an distro with SystemD, you probably won't need this
 
-if not [ -e /run/user/1000 ]
+set -x XDG_RUNTIME_DIR /run/user/1000
+
+if not test -e /run/user/1000 
     doas mkdir -p /run/user/1000
     doas chmod 700 /run/user/1000
     doas chown 1000 /run/user/1000
-    set -x XDG_RUNTIME_DIR /run/user/1000
 end
 
-# Some environment variables to make apps use Wayland natively
-# (In Fish you don't use export, you use "set -x variableName variableValue")
+# So I found out about set -U which sets "universal" variables. They persist even after reboot
+# This makes it unnecesary to set these environment variables each time Fish is executed
+# To permanently set an environment variable use "set -Ux variableName variableValue"
 
-set -x XDG_CURRENT_DESKTOP river
-set -x QT_QPA_PLATFORM wayland-egl
-set -x MOZ_ENABLE_WAYLAND 1
-set -x SDL_VIDEODRIVER wayland
-set -x ELM_DISPLAY wl
-set -x QT_QPA_PLATFORMTHEME qt5ct
+# set -x XDG_CURRENT_DESKTOP river
+# set -x QT_QPA_PLATFORM wayland-egl
+# set -x MOZ_ENABLE_WAYLAND 1
+# set -x SDL_VIDEODRIVER wayland
+# set -x ELM_DISPLAY wl
+# set -x QT_QPA_PLATFORMTHEME qt5ct
 
 # Little script that only runs if Fish runs interactively and as a login shell. 
 # If you have a display manager like GDM or LightDM, you won't need this
 # You can just press Return to start River. You can also type "Y" or "y" to start it
-# If you don't want to start River, just press Space or any letter before pressing Return
+# If you don't want to start River, just press Space or any letter
 
 if status is-login && status is-interactive && not pidof -q river
-     if test $boot
-         set upt (awk '{print int($1)}' /proc/uptime)
-         echo \n"Boot took $upt seconds"\n
-         set -x boot 1
-     end
      read response -n 1 -P "Launch River?"\n"> "
      switch $response
         case y Y ""
-            echo "Launching River..."
+            echo \n"Launching River..."\n
             dbus-run-session river 
         case '*'
-            echo "Not launching River..."
+            echo \n"Not launching River..."\n
 end
 end
 
